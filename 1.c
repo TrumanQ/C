088509fd6c0b199f46ac209e8ca66b2261dd2e98
Meng Qiu <truman_qiu@163.com>
2015-11-19 16:42:05 +0800

    tested rand(), srand() function

---
+++ 1.c
@@ -0,0 +1,18 @@
+//*  Test comment 
+
+#include <stdio.h>
+main()
+{
+  printf("Just one small step for coders. One giant leap for programmers!\n");
+
+ int iOperand1=0;
+ int iOperand2=0;
+
+ printf("\nEnter the First operand: ");
+ scanf("%d", &iOperand1);
+ printf("\nEnter the Second operand: ");
+ scanf("%d", &iOperand2);
+ printf("The result is %d.\n", iOperand1+iOperand2);
+
+ return 0;
+}
