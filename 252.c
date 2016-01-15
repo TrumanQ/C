1e38626793dab54e0550dbc0fed3c2ab13e5bb87
Meng Qiu <truman_qiu@163.com>
2016-01-15 16:45:26 +0800

    modified:   252.c :

--- 252.c
+++ 252.c
@@ -5,13 +5,14 @@ int main()
   FILE *pRead;
   char col1[20];
 
-  pRead = fopen("1.c", "r" );
+  pRead = fopen("names.dat", "r" );
 
   if ( pRead == NULL)
     printf ( "\nFile cannot be opened.\n" );
   else
     printf ( "\nFile opened for reading.\n" );
   fscanf ( pRead, "%s", col1 );
+  printf ( "\nDon\'t judge eof %s \n", col1 );
 
   while ( !feof ( pRead ) )
     {
